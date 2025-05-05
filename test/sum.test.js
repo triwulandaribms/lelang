aa7fb3a2d5b82f7dcc531d9d73a5a3c98d3ae19c
Tri Wulandari <triwulandari3456@gmail.com>
2025-05-05 20:54:51 +0700

    sh

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,7 @@
-import {sum} from "../src/sum.js";
+import { test, expect } from "bun:test";
+import { sum } from "../src/sum.js";
 
-test("test sum function", () =>{
-    const result = sum(1, 2)
-
-    expect (result).toBe(3);
+test("test sum function", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
 });
